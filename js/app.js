93f136de439be5a64e2c1b0adaa46f3b07d41a94
phillipj1514 <phillipjunior95@gmail.com>
2019-11-10 23:39:21 -0500

    final draft

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
+const name = document.getElementById("name");
+const results = document.getElementById("result");
 document.addEventListener("DOMContentLoaded", initiate);
 function initiate(){
     document.getElementById("search").addEventListener("click", getData);
+    results.classList.remove("alert");
 }
 function getData(){
-    //alert("Your Data is");
-    const name = document.getElementById("name").value;
-    //alert(name);
     const superhero = new XMLHttpRequest();
     superhero.onreadystatechange = function (){
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            document.getElementById("result").innerHTML = this.responseText;
+            result.innerHTML = this.responseText;
+            if( this.responseText != "Superhero not found"){
+                results.classList.remove("alert-danger");
+            }else{
+                results.classList.add("alert-danger");
+            }
+
         }
     };
 
-    superhero.open("GET", "superheroes.php?query="+name,true);
+    superhero.open("GET", "superheroes.php?query="+name.value,true);
     superhero.send();
+    name.value = "";
     
 }
 
